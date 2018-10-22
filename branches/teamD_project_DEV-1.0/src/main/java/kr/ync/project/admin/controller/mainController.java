e517e115aca787dcb705008b5ec785f1a142a4b9
team_d <team_d@4739bca4-1698-402d-ba8e-b476c1a9c1bb>
2018-10-22 03:17:55 +0000

    #이우호 admin 홈페이지 수정 -2

---
+++ branches/teamD_project_DEV-1.0/src/main/java/kr/ync/project/admin/controller/mainController.java
@@ -0,0 +1,27 @@
+package kr.ync.project.admin.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.extern.slf4j.Slf4j;
+@Controller
+@Slf4j
+@RequestMapping("/admin")
+public class mainController {
+	
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+
+		// 가나다
+		log.info("teamD log 내용", locale); 
+
+				
+		return "admin/home";
+	}
+}
