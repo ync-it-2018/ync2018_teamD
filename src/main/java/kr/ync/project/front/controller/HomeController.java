a69fc4b8069f94d4eb7b49e8b797ca4181366643
team_d <team_d@4739bca4-1698-402d-ba8e-b476c1a9c1bb>
2018-10-22 03:52:08 +0000

    #김건우 템플릿 수정 0005 - loginform.css 파일을 /resource/front/css 경로에 추가 및 index.jsp 내부에 Login 틀, SearchResult 항목 추가
    HomeController.java, PageController.java 통일화

--- src/main/java/kr/ync/project/front/controller/HomeController.java
+++ src/main/java/kr/ync/project/front/controller/HomeController.java
@@ -1,39 +1,25 @@
 package kr.ync.project.front.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
+@Slf4j
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	@RequestMapping(value = "/")
+	public String home() {
 
 		// 가나다
-		logger.info("teamD log 내용", locale); 
-
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
+		log.info("index call....."); 
 		
 		return "front/index";
 	}
--- src/main/java/kr/ync/project/front/controller/PageController.java
+++ src/main/java/kr/ync/project/front/controller/PageController.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 public class PageController {
 	
 		
-	@GetMapping("/blog")
+	@RequestMapping("/blog")
 	public String blog() {
 		log.info("blog call.....");
 		
