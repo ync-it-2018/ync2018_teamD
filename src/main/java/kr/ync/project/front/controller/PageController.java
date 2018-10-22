ebc5190ece8b480a363d99471c7dba9dcf85cdf1
team_d <team_d@4739bca4-1698-402d-ba8e-b476c1a9c1bb>
2018-10-22 00:39:27 +0000

    #문태일 dev admin, front 패키지 생성

--- src/main/java/kr/ync/project/front/controller/PageController.java
+++ src/main/java/kr/ync/project/front/controller/PageController.java
@@ -1,4 +1,4 @@
-package kr.ync.project.controller;
+package kr.ync.project.front.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
