4245c4bf49bb1968f270e08521e2f70b1f53e35e
team_d <team_d@4739bca4-1698-402d-ba8e-b476c1a9c1bb>
2018-10-22 02:14:27 +0000

    #이우호 admin 홈페이지 수정

---
+++ src/main/java/kr/ync/project/admin/service/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package kr.ync.project.admin.service;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.ync.project.admin.domain.UserVO;
+import kr.ync.project.admin.dto.LoginDTO;
+import kr.ync.project.admin.persistence.UserDAO;
+
+
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserDAO dao;
+
+	@Override
+	public UserVO login(LoginDTO dto) throws Exception {
+
+		return dao.login(dto);
+	}
+
+	@Override
+	public void keepLogin(String uids, String sessionId, Date next) throws Exception {
+
+		dao.keepLogin(uids, sessionId, next);
+
+	}
+
+	@Override
+	public UserVO checkLoginBefore(String value) {
+
+		return dao.checkUserWithSessionKey(value);
+	}
+}
