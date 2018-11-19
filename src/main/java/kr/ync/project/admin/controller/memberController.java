fc7db23859b232767ce38f60ff2d81a3ff812583
team_d <team_d@4739bca4-1698-402d-ba8e-b476c1a9c1bb>
2018-11-19 10:34:07 +0000

    #문태일 회원관리-회원목록 출력

--- src/main/java/kr/ync/project/admin/controller/memberController.java
+++ src/main/java/kr/ync/project/admin/controller/memberController.java
@@ -11,21 +11,35 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import kr.ync.project.admin.domain.memberVO;
+import kr.ync.project.admin.service.memberService;
 //import kr.ync.project.admin.domain.UserVO;
 //import kr.ync.project.admin.dto.LoginDTO;
 //import kr.ync.project.admin.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+/**
+ * memberController.java
+ * 
+ * 
+ * @Author : 문태일
+ * @Date   : 2018. 11. 19.
+ * @Description
+ * 
+ * 
+ */
 @Controller
 @Slf4j
 @RequestMapping("/admin")
 public class memberController {
 	
 	@Inject
-//	private UserService service;
+	private memberService service;
 	
 	@RequestMapping(value = "/memberList", method = RequestMethod.GET)
-	public String memberList() {
+	public String memberList(memberVO board, Model model) throws Exception {
 		log.info("member List call.....");
+		
+		model.addAttribute("list", service.listAll());
 			
 		return "admin/member/memberList";
 	}	
