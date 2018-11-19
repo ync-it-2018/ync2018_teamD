bc870b552c6433155f25af6b1b430d18b2eb6907
team_d <team_d@4739bca4-1698-402d-ba8e-b476c1a9c1bb>
2018-11-19 02:00:48 +0000

    #이우호 admin 호텔 페이지 목록 동작

---
+++ branches/teamD_project_DEV-1.0/src/main/java/kr/ync/project/admin/service/BoardService.java
@@ -0,0 +1,26 @@
+package kr.ync.project.admin.service;
+
+import java.util.List;
+
+import kr.ync.project.admin.domain.BoardVO;
+
+public interface BoardService {
+
+      public void regist(BoardVO board) throws Exception;
+      
+      public BoardVO read(Integer bno) throws Exception;
+      
+      public void modify (BoardVO board) throws Exception;
+      
+      public void remove (Integer bno) throws Exception;
+      
+      public List<BoardVO> listAll() throws Exception;
+
+//      public List<BoardVO> listCriteria(Criteria cri) throws Exception;
+//      
+//      public int listCountCriteria(Criteria cri) throws Exception;
+//      
+//      public List<BoardVO> listSearchCriteria(SearchCriteria cri) throws Exception;
+//      
+//      public int listSearchCount(SearchCriteria cri) throws Exception;
+}
