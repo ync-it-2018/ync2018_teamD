df696392b61f474b211ed7f7f1a9c4058603064e
team_d <team_d@4739bca4-1698-402d-ba8e-b476c1a9c1bb>
2018-12-14 06:15:48 +0000

    #문태일 FAQ paging, 삭제시 비활성처리, 주석

--- src/main/java/kr/ync/project/admin/persistence/FaqDAO.java
+++ src/main/java/kr/ync/project/admin/persistence/FaqDAO.java
@@ -7,5 +7,9 @@ import kr.ync.project.admin.domain.FaqVO;
 public interface FaqDAO {
 
 	public List<FaqVO> FaqList() throws Exception;
+
+	public int FaqCount() throws Exception;
+
+	public void FAQDelete(String faq_idx) throws Exception;
 	
 }
